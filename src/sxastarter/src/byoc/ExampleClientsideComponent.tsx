e1c0ed56b08fc53bea30357bc2e07aeb0260735e
Timotheos Lanitis <timos.lanitis@sitecore.com>
2024-01-31 10:47:49 +0200

    commit

--- src/sxastarter/src/byoc/ExampleClientsideComponent.tsx
+++ src/sxastarter/src/byoc/ExampleClientsideComponent.tsx
@@ -1,13 +1,13 @@
-"use client";
-import * as FEAAS from "@sitecore-feaas/clientside/react";
-import { useEffect, useState } from "react";
+'use client';
+import * as FEAAS from '@sitecore-feaas/clientside/react';
+import { ReactNode, useEffect, useState } from 'react';
 
 export default function ExampleClientsideComponent(props: {
   firstName: string;
   lastName?: string;
   telephone?: string;
   bold?: boolean;
-  children?: any;
+  children?: ReactNode;
 }) {
   const [counter, setCounter] = useState(0);
 
@@ -21,7 +21,7 @@ export default function ExampleClientsideComponent(props: {
   return (
     <>
       <h2>Clientside</h2>
-      <dl style={props.bold ? { fontWeight: "bold" } : {}}>
+      <dl style={props.bold ? { fontWeight: 'bold' } : {}}>
         <dt>Description</dt>
         <dd>Interactive UI</dd>
         <dt>Rendered on</dt>
@@ -34,7 +34,7 @@ export default function ExampleClientsideComponent(props: {
         <dd>
           <var>{counter}</var>s elapsed
         </dd>
-        {props.children && props.children.length != 0 && (
+        {props.children && (props.children as ReactNode[]).length != 0 && (
           <>
             <dt>Children</dt>
             <dd>{props.children}</dd>
@@ -46,40 +46,39 @@ export default function ExampleClientsideComponent(props: {
 }
 
 FEAAS.registerComponent(ExampleClientsideComponent, {
-  name: "clientside-only",
-  title: "Clientside-only component",
-  description: "Description of my example component",
-  thumbnail:
-    "https://feaasstatic.blob.core.windows.net/assets/thumbnails/byoc.svg",
-  group: "Examples",
-  required: ["firstName"],
+  name: 'clientside-only',
+  title: 'Clientside-only component',
+  description: 'Description of my example component',
+  thumbnail: 'https://feaasstatic.blob.core.windows.net/assets/thumbnails/byoc.svg',
+  group: 'Examples',
+  required: ['firstName'],
   properties: {
     firstName: {
-      type: "string",
-      title: "First name",
+      type: 'string',
+      title: 'First name',
     },
     lastName: {
-      type: "string",
-      title: "Last name",
+      type: 'string',
+      title: 'Last name',
     },
     telephone: {
-      type: "number",
-      title: "Telephone",
+      type: 'number',
+      title: 'Telephone',
       minLength: 10,
     },
     bold: {
-      type: "boolean",
-      title: "Show text in bold weight",
+      type: 'boolean',
+      title: 'Show text in bold weight',
     },
   },
   ui: {
     firstName: {
-      "ui:autofocus": true,
-      "ui:emptyValue": "",
-      "ui:placeholder": "Write your first name",
+      'ui:autofocus': true,
+      'ui:emptyValue': '',
+      'ui:placeholder': 'Write your first name',
     },
     bold: {
-      "ui:widget": "radio",
+      'ui:widget': 'radio',
     },
   },
 });
